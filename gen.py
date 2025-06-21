1cddbfa9d13b15195b431030d7ebdd1ecba5092f
Aidan Busby <aidanbusby123@gmail.com>
2025-06-20 17:34:38 -0700

    worked on prompting

--- gen.py
+++ gen.py
@@ -21,10 +21,9 @@ gemini_api_key = os.environ["GEMINI_API_KEY"]
 gemini_client = genai.Client(api_key=gemini_api_key)
 
 parser = argparse.ArgumentParser(description="tool to generate questions for RocketPrepAI")
-parser.add_argument("outputfile", help="Name of output file")
-parser.add_argument("--topics", nargs="+", choices=["all", "words_in_context", "text_structure_and_purpose"], default="all")
+parser.add_argument("--domains", nargs="+", choices=["all", "craft_and_structure", "information_and_ideas", "standard_english_conventions", "expression_of_ideas"], default="all")
 parser.add_argument("--difficulty", nargs="+", choices=["easy", "medium", "hard"], default="all")
-parser.add_argument("--batch_number", default=4)
+parser.add_argument("--batch_number", default=1)
 parser.add_argument("--epochs", default=1)
 
 args = parser.parse_args()
@@ -36,12 +35,47 @@ db = firestore.client()
 
 base_user_prompt = "Please generate a question, where the correct answer is {random_choice} and the difficulty is {difficulty}"
 
+central_ideas_and_details_samples = gemini_client.files.upload(file="docs/centralideas.pdf")
+
 def load_prompts():
+
+    # Craft and Structure
     with open ("prompts/craft_and_structure/words_in_context.txt", "r") as f:
         words_in_context_prompt = f.read()
 
     with open ("prompts/craft_and_structure/text_structure_and_purpose.txt", "r") as f:
         text_structure_and_purpose_prompt = f.read()
+
+    with open ("prompts/craft_and_structure/cross_text_connections.txt", "r") as f:
+        cross_text_connections_prompt = f.read()
+
+    
+    # Information and Ideas
+    with open ("prompts/information_and_ideas/central_ideas_and_details.txt", "r") as f:
+        central_ideas_and_details_prompt = f.read()
+
+    with open ("prompts/information_and_ideas/command_of_evidence.txt" , "r") as f:
+        command_of_evidence_prompt = f.read()
+
+    with open ("prompts/information_and_ideas/inferences.txt", "r") as f:
+        inferences_prompt = f.read()
+
+
+    # Standard English Conventions
+
+    with open ("prompts/standard_english_conventions/boundaries.txt", "r") as f:
+        boundaries_prompt = f.read()
+
+    with open ("prompts/standard_english_conventions/form_structure_and_sense.txt", "r") as f:
+        form_structure_and_sense_prompt = f.read()
+
+    # Expression of Ideas
+
+    with open ("prompts/expression_of_ideas/rhetorical_synthesis.txt", "r") as f:
+        rhetorical_synthesis_prompt = f.read()
+
+    with open("prompts/expression_of_ideas/transitions.txt", "r") as f:
+        transitions_prompt = f.read()
 
     with open ("prompts/mainprompt.txt", "r") as f:
         main_prompt = f.read()
@@ -50,45 +84,66 @@ def load_prompts():
         format_prompt = f.read()
     
 
-    return {"craft_and_structure_prompts": {"words_in_context": words_in_context_prompt, "text_structure_and_purpose": text_structure_and_purpose_prompt}, "main_prompt": main_prompt, "format_prompt": format_prompt}
+    return {"craft_and_structure": {"words_in_context": words_in_context_prompt, "text_structure_and_purpose": text_structure_and_purpose_prompt, "cross_text_connections": cross_text_connections_prompt}, "information_and_ideas":{"central_ideas_and_details": central_ideas_and_details_prompt, "command_of_evidence": command_of_evidence_prompt, "inferences": inferences_prompt}, "standard_english_conventions": {"boundaries": boundaries_prompt, "form_structure_and_sense": form_structure_and_sense_prompt}, "expression_of_ideas": {"rhetorical_synthesis": rhetorical_synthesis_prompt, "transitions": transitions_prompt}, "main_prompt": main_prompt, "format_prompt": format_prompt}
 
 prompts = load_prompts()
 
-def format_question(raw_data):
+def format_question(raw_data, domain, skill_category, difficulty):
     format_prompt = prompts["format_prompt"]
+    format_prompt = format_prompt.format(domain=domain, skill_category=skill_category, difficulty=difficulty)
     format_messages = [{"role": "system", "content": format_prompt}]
     format_messages.append({"role": "user", "content": f"Please format this for me: {raw_data}"})
-    response = chatgpt_client.chat.completions.create(
-        model="gpt-3.5-turbo",
+    '''response = chatgpt_client.chat.completions.create(
+        model="gpt-4.1-mini",
         messages=format_messages
+    )'''
+
+    gemini_response = gemini_client.models.generate_content(
+        model="gemini-2.5-pro-preview-06-05",
+        contents=f"Please format this for me: {raw_data}",
+        config=GenerateContentConfig(
+            system_instruction=[format_prompt]
+        ),
     )
 
-    response = response.choices[0].message.content
+    #response = response.choices[0].message.content
+    gemini_response = gemini_response.text
+    print(gemini_response)
+    return gemini_response
 
-    return response
-
-def generate_question(system_prompt, user_prompt, messages):
+def generate_question(system_prompt, user_prompt, messages, domain, skill_category, difficulty):
     messages.append({"role": "system", "content": system_prompt})
     messages.append({"role": "user", "content": user_prompt})
 
-    response = chatgpt_client.chat.completions.create(
+    ''' response = chatgpt_client.chat.completions.create(
         model="o4-mini",
         messages=messages,
         temperature=1,
     )
+    '''
+    if (skill_category == 'central_ideas_and_details'):
+        gemini_response = gemini_client.models.generate_content(
+            model="gemini-2.5-pro-preview-06-05",
+            contents=user_prompt,
+            config=GenerateContentConfig(
+                system_instruction=[system_prompt],
+                temperature=1.2
+            ),
+        )
+    else: 
+        gemini_response = gemini_client.models.generate_content(
+            model="gemini-2.5-pro-preview-06-05",
+            contents=[central_ideas_and_details_samples, user_prompt],
+            config=GenerateContentConfig(
+                system_instruction=[system_prompt],
+                temperature=1.2
+            ),
+        )
 
-    gemini_response = gemini_client.models.generate_content(
-        model="gemini-2.5-pro-preview-06-05",
-        contents=user_prompt,
-        config=GenerateContentConfig(
-            system_instruction=[system_prompt]
-        ),
-    )
+    #response = response.choices[0].message.content
+    #messages.append({"role": "assistant", "content": response})
 
-    response = response.choices[0].message.content
-    messages.append({"role": "assistant", "content": response})
-
-    formatted_response = format_question(gemini_response)
+    formatted_response = format_question(gemini_response, domain=domain, skill_category=skill_category, difficulty=difficulty)
     
     question = re.search(r'\{.*\}', formatted_response, re.DOTALL)
 
@@ -110,8 +165,12 @@ def add_question(question):
 def generate():
     chatgpt_messages = []
 
+    q_num = 0
 
+    difficulties = ["easy", "medium", "hard"]
     main_prompt = prompts["main_prompt"] # main prompt for SAT questions
+
+    print(main_prompt)
 
     if args.difficulty == "all":
         difficulties = ["easy", "medium", "hard"]
@@ -121,23 +180,26 @@ def generate():
     #print (args.topics)
     for epoch in range (0, args.epochs):
         print(f"Epoch: {epoch}")
-        for batch in range (0, args.batch_number):
+        for batch in range (0, int(args.batch_number)):
             for domain, skills in prompts.items():
                 #print (skills)
                 for dif in difficulties:
-                    if domain != "main_prompt":
+                    if domain != "main_prompt" and domain != "format_prompt" and domain != "gen_prompt" and ((args.domains == "all") or  (domain in args.domains)):
                         print(f"Domain: {domain}") 
+                        dif = str(dif)
                         
                         for skill_key, skill_value in skills.items():
                            # print(skill_key)
-                            if (args.topics == "all" or skill_key in args.topics):
                                 print(f"Skill: {skill_key}")
-                                skill_prompt = skill_value.format(difficulty=dif)
-                                system_prompt = main_prompt.format(domain=domain, skill_category=skill_key, formula=skill_prompt)
+                                skill_prompt = skill_value
+                                system_prompt = main_prompt.format(domain=domain, skill_category=skill_key, formula=skill_prompt, difficulty=dif)
+                                #print(system_prompt)
                                 random_choice = random.choice(["A", "B", "C", "D"])
                                 user_prompt = base_user_prompt.format(difficulty=dif, random_choice=random_choice)
-                                question = generate_question(system_prompt, user_prompt, chatgpt_messages)
-                                add_question(question)
+                                question = generate_question(system_prompt, user_prompt, chatgpt_messages, domain=domain, skill_category=skill_key, difficulty=dif)
+                                question_id = add_question(question)
+                                print(f"Question #{q_num} , id {question_id} generated!")
+                                q_num+=1
                                 #print(question)
 
 
